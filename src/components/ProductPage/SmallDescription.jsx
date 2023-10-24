c324d9d8966334f8a48884658581637aea8266c9
samxaradzenika <110243055+samxaradzenika@users.noreply.github.com>
2023-10-25 02:43:44 +0400

    s

--- src/components/ProductPage/SmallDescription.jsx
+++ src/components/ProductPage/SmallDescription.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 import {Button} from "antd";
+import view from "../../assets/view.svg";
+import download from "../../assets/download.svg";
 const SmallDescription = ({data}) => {
+  console.log(data);
   return (
     <>
       <Container>
@@ -20,10 +23,31 @@ const SmallDescription = ({data}) => {
         <div style={{display: "flex", gap: "20px", flexDirection: "row"}}>
           <span>Package Sizes : </span>
           {data?.packageSizes.map((item) => (
-            <div>
+            <div key={item}>
               <Button size="middle" type="primary">
                 {item}
               </Button>
+            </div>
+          ))}
+        </div>
+        <div style={{display: "flex", gap: "20px", flexDirection: "row"}}>
+          {data?.pdfUrls?.map((item) => (
+            <div style={{width: "200px"}}>
+              <div>
+                <Button size="large">{item.viscosityGrade}</Button>
+              </div>
+              <div style={{width: "100%"}}>
+                <Button
+                  style={{width: "50px"}}
+                  icon={<img src={view} alt="view" />}
+                  type="dashed"
+                />
+                <Button
+                  icon={<img src={download} alt="view" />}
+                  size="medium"
+                  type="dashed"
+                />
+              </div>
             </div>
           ))}
         </div>
@@ -40,7 +64,7 @@ const Container = styled.div`
   justify-content: center;
   gap: 25px;
   margin: 0 auto;
-
+  margin-bottom: 30px;
   color: #555;
   span {
     color: black;
