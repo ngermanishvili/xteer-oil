5e13d9611175cdd8a22eae7c30bb19ac31268977
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-10-25 16:39:14 +0400

    ssss

--- src/zustand/store.jsx
+++ src/zustand/store.jsx
@@ -8,7 +8,8 @@ export const dataStore = create((set) => ({
   loading: true,
   fetchData: async () => {
     try {
-      const { data } = await axios.get("http://localhost:8000/oils");
+      const { data } = await axios.get("http://3.87.31.172:8000/oils");
+      console.log(data);
       set({ data: data, error: null, loading: false });
     } catch (error) {
       console.error("Error fetching data:", error);
