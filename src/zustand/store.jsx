b8f255c222b003e46e0ce6cba57d2f4a4b3a080a
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-10-27 04:56:00 +0400

    update backend updated succesfuly

--- src/zustand/store.jsx
+++ src/zustand/store.jsx
@@ -8,7 +8,7 @@ export const dataStore = create((set) => ({
   loading: true,
   fetchData: async () => {
     try {
-      const { data } = await axios.get("http://localhost:8000/oils");
+      const { data } = await axios.get("https://gentoback.onrender.com/oils");
       set({ data: data, error: null, loading: false });
     } catch (error) {
       console.error("Error fetching data:", error);
