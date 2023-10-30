40345a832e6aec81bdf8c12d7ae04361e1487aed
Tornike Samkharadze <100222618+Tornikesamkharadze@users.noreply.github.com>
2023-10-30 20:05:41 +0400

    Update SmallDescription.jsx

--- src/components/ProductPage/SmallDescription.jsx
+++ src/components/ProductPage/SmallDescription.jsx
@@ -1,77 +1,82 @@
 import React from "react";
 import styled from "styled-components";
-import {Button} from "antd";
-import {useTranslation} from "react-i18next";
-
+import { Button } from "antd";
+import { useTranslation } from "react-i18next";
 import DownloadPds from "./DownloadPds";
-const SmallDescription = ({data}) => {
-  const {t} = useTranslation();
+
+const SmallDescription = ({ data }) => {
+  const { t } = useTranslation();
+
   return (
-    <>
-      <Container>
-        <h2>
-          <span>{t("ProductName")}&nbsp; :&nbsp; </span> {data?.productName}
-        </h2>
-        <h3>
-          <span>{t("Category")}&nbsp; :&nbsp; </span>
-          {data?.category}
-        </h3>
-        <h2>
-          <span>{t("ProductLine")}&nbsp; :&nbsp; </span>
-          {data?.productLine}
-        </h2>
-        <p>{data?.description}</p>
-        <div style={{display: "flex", gap: "10px", flexDirection: "row"}}>
-          <span style={{minWidth: "170px"}}>
-            <b>{t("PackageSizes")}&nbsp; :&nbsp;</b>
-          </span>
-          <div
-            style={{
-              display: "flex",
-              gap: "10px",
-              flexDirection: "row",
-              flexWrap: "nowrap",
-              overflowX: "auto",
-              overflowY: "hidden",
-            }}
-          >
-            {data?.packageSizes.map((item) => (
-              <div key={item}>
-                <Button size="middle" type="primary">
-                  {item}
-                </Button>
-              </div>
-            ))}
-          </div>
-        </div>
-        <DownloadPds data={data} />
-      </Container>
-    </>
+    <Container>
+      <h2>
+        <span>{t("ProductName")}&nbsp; :&nbsp; </span> {data?.productName}
+      </h2>
+      <h3>
+        <span>{t("Category")}&nbsp; :&nbsp; </span>
+        {data?.category}
+      </h3>
+      <h2>
+        <span>{t("ProductLine")}&nbsp; :&nbsp; </span>
+        {data?.productLine}
+      </h2>
+      <p>{data?.description}</p>
+      <FlexContainer>
+        <span style={{ minWidth: "145px" }}>{t("PackageSizes")}&nbsp; :</span>
+        <PackageSizesContainer>
+          {data?.packageSizes.map((item) => (
+            <div key={item}>
+              <Button size="middle" type="primary">
+                {item}
+              </Button>
+            </div>
+          ))}
+        </PackageSizesContainer>
+      </FlexContainer>
+      <DownloadPds data={data} />
+    </Container>
   );
 };
 
 const Container = styled.div`
   width: 100%;
-  font-size: 20px;
+  font-size: 18px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   gap: 25px;
   margin: 0 auto;
   margin-bottom: 30px;
-
   color: #555;
+
   @media (max-width: 1160px) {
     margin-bottom: 0px;
   }
+
   span {
     color: black;
     font-weight: 700;
   }
+
   @media (max-width: 1160px) {
     font-size: 18px;
     text-align: left;
   }
+`;
+const FlexContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: baseline;
+`;
+
+const PackageSizesContainer = styled.div`
+  display: flex;
+  gap: 10px;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  overflow-x: auto;
+  overflow-y: hidden;
+  padding-bottom: 15px;
 `;
 
 export default SmallDescription;
