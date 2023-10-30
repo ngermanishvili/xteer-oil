4189a457b8728f60d48ac6617a66099fa09df730
Tornike Samkharadze <100222618+Tornikesamkharadze@users.noreply.github.com>
2023-10-30 20:21:15 +0400

    Update SmallDescription.jsx

--- src/components/ProductPage/SmallDescription.jsx
+++ src/components/ProductPage/SmallDescription.jsx
@@ -22,7 +22,7 @@ const SmallDescription = ({ data }) => {
       </h2>
       <p>{data?.description}</p>
       <FlexContainer>
-        <span style={{ minWidth: "145px" }}>{t("PackageSizes")}&nbsp; :</span>
+        <span style={{ minWidth: "155px" }}>{t("PackageSizes")}&nbsp; :</span>
         <PackageSizesContainer>
           {data?.packageSizes.map((item) => (
             <div key={item}>
